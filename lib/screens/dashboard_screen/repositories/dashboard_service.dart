f8f70fdab030047d91b1d5e5e80f3c472cac0b74
Surya Permana <suryapermana92@gmail.com>
2020-07-28 23:39:13 +0700

    add dashboard services

---
+++ lib/screens/dashboard_screen/repositories/dashboard_service.dart
@@ -0,0 +1,23 @@
+import 'package:fluttersismic/global/services/auth.dart';
+import 'package:fluttersismic/utils/constants.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+String api_base_url = Constants.api_base_url;
+
+class DashboardService {
+  Future<http.Response> getDashboardData() async {
+    String accessToken = await authenticationService.getCurrentUser();
+
+    var url = "$api_base_url/1/dashboard";
+    Map<String, String> headers = {
+      'Authorization': 'Bearer $accessToken',
+      'Accept': 'application/json',
+    };
+    final response = await http.get(url, headers: headers);
+    return response;
+  }
+}
+
+DashboardService dashboardService = DashboardService();
